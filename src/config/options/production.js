1d804ba10e07678f46efc6f6cdeee653c2617f2a
AVANADE-CORP\renan.de.souza.pinto <renan.de.souza.pinto@avanade.com>
2019-04-25 15:41:38 -0300

    parsing postgres credentials

--- src/config/options/production.js
+++ src/config/options/production.js
@@ -1,23 +1,27 @@
 import * as path from 'path';
+import * as PostgressConnectionStringParser from 'pg-connection-string';
 import TypeOrmConfigFactory from '../orm/typeorm/typeorm-config-factory';
+
+const connectionOptions = PostgressConnectionStringParser.parse(process.env.DATABASE_URL);
+
 
 // const dotenv = require('dotenv');
 
 // dotenv.load();
 
 const config = {
-  database: process.env.DATABASE_URL || process.env.DATABASE_NAME,
+  database: process.env.DATABASE_NAME || connectionOptions.database,
+  host: process.env.DATABASE_HOST || connectionOptions.host,
+  username: process.env.DATABASE_USER || connectionOptions.user,
+  password: process.env.DATABASE_PASSWORD || connectionOptions.password,
+  port: process.env.DATABASE_PORT || connectionOptions.port || 5432,
   entities: [path.resolve(__dirname, '../../infrastructure/repositories/typeorm/schema/**/index.js')],
   extra: {
     max: process.env.DATABASE_POOL_SIZE || 5,
   },
-  host: process.env.DATABASE_HOST,
   logging: true,
   name: 'default',
-  password: process.env.DATABASE_PASSWORD,
-  port: process.env.DATABASE_PORT,
   type: 'postgres',
-  username: process.env.DATABASE_USER,
 };
 
 const dbConfig = new TypeOrmConfigFactory(config).create();
